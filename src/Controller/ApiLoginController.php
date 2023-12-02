f61f8069cbf58102125d6417646d3eff231ca8ce
thibaud <thibaud.mallet@hotmail.fr>
2023-12-02 07:26:36 +0100

    WIP - Configure JWT Token

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'app_api_login')]
     public function index():JsonResponse {
 
-        var_dump($this->getUser());
-        die();
         $user = $this->getUser();
         assert($user instanceof User);
         
